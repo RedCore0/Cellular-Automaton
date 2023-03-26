95f1a0bdcb89e4652e9f71aa742566d662d90f28
RedCore0 <redcore04gg@gmail.com>
2023-03-26 21:38:59 +0100

    GameOfLiveV2
    improved user experience
    apility to modify board size
    exceptions handled

--- GameOfLife.py
+++ GameOfLife.py
@@ -8,29 +8,47 @@ class Cell:
 
 Board = []
 def CreateBoard():
+    global BoardSize
+    try:
+        BoardSize = int(input("Board size   MIN(10)/MAX(100): "))
+    except:
+        CreateBoard()
+        return
     RowRecursion = 0
-    while RowRecursion < 10:
-        Column = []
-        ColumnRecursion = 0
-        while ColumnRecursion < 10:
-            NewCell = Cell(False, " - ")
-            Column.append(NewCell)
-            ColumnRecursion+=1
-        Board.append(Column)
-        RowRecursion+=1
+    if BoardSize > 9 and BoardSize < 101:
+        while RowRecursion < BoardSize:
+            Column = []
+            ColumnRecursion = 0
+            while ColumnRecursion < BoardSize:
+                NewCell = Cell(False, " · ")
+                Column.append(NewCell)
+                ColumnRecursion+=1
+            Board.append(Column)
+            RowRecursion+=1
+        ColumnIndex()
+    else: CreateBoard()
 
 def CreateLife():
     Continue = True
     while Continue:
-        y = int(input("Row: "))
-        x = int(input("Column: "))
+        try:
+            y = int(input("Row: "))
+            x = int(input("Column: "))
+        except:
+            continue
         Board[y][x].alive = True
-        Board[y][x].visual = " O "
-        DoContinue = input("Choose another cell?    y/n \n: ")
-        if(DoContinue == "y"):
-            pass
-        elif(DoContinue == "n"):
-            Continue = False
+        Board[y][x].visual = " ■ "
+        RepeatInput = True
+        while(RepeatInput):
+            DoContinue = input("Choose another cell?    y/n \n: ")
+            if(DoContinue == "y"):
+                PrintBoard()
+                RepeatInput = False
+            elif(DoContinue == "n"):
+                RepeatInput = False
+            else:
+                print("invalid input")
+
     PrintBoard()
     time.sleep(1)
 
@@ -42,7 +60,7 @@ def LifeSimulation():
             LivingCells = 0
             if r.index(c)-1 > -1 and r[r.index(c)-1].alive == True:
                 LivingCells+=1
-            if r.index(c)+1 < 10 and r[r.index(c)+1].alive == True:
+            if r.index(c)+1 < BoardSize and r[r.index(c)+1].alive == True:
                 LivingCells+=1
 
             UpBoard = Board.index(r)-1
@@ -51,33 +69,51 @@ def LifeSimulation():
 
             for row in AdjacentRows:
                 if row == UpBoard and UpBoard < 0: continue
-                elif DownBoard > 9: continue
+                elif DownBoard > BoardSize-1: continue
 
                 if Board[row][r.index(c)].alive == True:
                     LivingCells+=1
                 if Board[row][r.index(c)-1].alive == True:
                     LivingCells+=1
-                if r.index(c)+1<10 and Board[row][r.index(c)+1].alive == True:
+                if r.index(c)+1<BoardSize and Board[row][r.index(c)+1].alive == True:
                     LivingCells+=1
 
             if LivingCells < 2 or LivingCells > 3:
                 ToUpdate[Board.index(r)][r.index(c)].alive = False
-                ToUpdate[Board.index(r)][r.index(c)].visual = " - "
+                ToUpdate[Board.index(r)][r.index(c)].visual = " · "
             elif LivingCells == 3:
                 ToUpdate[Board.index(r)][r.index(c)].alive = True
-                ToUpdate[Board.index(r)][r.index(c)].visual = " O "
+                ToUpdate[Board.index(r)][r.index(c)].visual = " ■ "
     Board = ToUpdate
     PrintBoard()
             
 def StartLifeSimulation():
     while True:
         LifeSimulation()
-        time.sleep(1)
+        time.sleep(.5)
+
+ColumnCount = ""
+def ColumnIndex():
+    print(BoardSize)
+    global ColumnCount
+    Index = 0
+    while(Index < BoardSize):
+        Number = ""
+        if Index < 10:
+            Number = (" " + str(Index) + " ")
+        else:
+            Number = (" "+ str(Index))
+        ColumnCount+=str(Number)
+        Index+=1
 
 def PrintBoard():
     os.system("cls")
+    RowID = 0
     for r in Board:
-        print("\n", end="")
+        if Board.index(r) == 0: print("    ",ColumnCount,"\n", RowID, "  ", end="")
+        elif Board.index(r) < 10: print("\n", RowID, "  ", end="")
+        else: print("\n", RowID, " ",end="")
+        RowID+=1
         for c in r:
             print(c.visual, end="")
     print("")
